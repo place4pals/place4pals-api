1b386cb8605607ade6addeaf9667d0834df5bc82
Chris Aitken <chris@heythisischris.com>
2021-08-03 00:11:31 -0400

    Moved token generation to top of script. Handling email confirmation via public lambda endpoint. Setting preferred_username cognito attribute after confirming signup (allows username login).

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const aws = require('aws-sdk');
 const { Pool } = require('pg');
 const poolConfig = {
     user: process.env.user,
@@ -8,17 +9,10 @@ const poolConfig = {
 };
 
 exports.handler = async(event, context) => {
-    console.log("place4pals init");
-    console.log(event);
+    console.log("place4pals init", event);
+    event.body ? event.body = JSON.parse(event.body) : null;
 
-    if (event.triggerSource === 'PostConfirmation_ConfirmSignUp') {
-        let pool = new Pool(poolConfig);
-        let response = await pool.query('INSERT INTO users(id, email, username, user_type) VALUES($1, $2, $3, $4) RETURNING *', [event.request.userAttributes['sub'], event.request.userAttributes['email'], event.request.userAttributes['custom:username'], 'pal']);
-        console.log(response);
-        pool.end();
-        return context.done(null, event);
-    }
-    else if (['TokenGeneration_Authentication', 'TokenGeneration_RefreshTokens'].includes(event.triggerSource)) {
+    if (['TokenGeneration_Authentication', 'TokenGeneration_RefreshTokens'].includes(event.triggerSource)) {
         event.response = {
             "claimsOverrideDetails": {
                 "claimsToAddOrOverride": {
@@ -32,5 +26,61 @@ exports.handler = async(event, context) => {
             }
         };
         return context.done(null, event);
+    }
+    else if (event.triggerSource === 'PostConfirmation_ConfirmSignUp') {
+        let pool = new Pool(poolConfig);
+        let response = await pool.query('INSERT INTO users(id, email, username, user_type) VALUES($1, $2, $3, $4) RETURNING *', [event.request.userAttributes['sub'], event.request.userAttributes['email'], event.request.userAttributes['custom:username'], event.request.userAttributes['custom:userType']]);
+        console.log(response);
+        pool.end();
+
+        const cisp = new aws.CognitoIdentityServiceProvider();
+        await cisp.adminUpdateUserAttributes({
+            UserAttributes: [{
+                Name: 'preferred_username',
+                Value: event.request.userAttributes['custom:username']
+            }],
+            UserPoolId: process.env.userPoolId,
+            Username: event.userName
+        }).promise();
+
+        return context.done(null, event);
+    }
+    else if (event.triggerSource === 'CustomMessage_SignUp') {
+        event.response.emailSubject = `Confirm your registration, ${event.request.userAttributes['custom:username']}!`;
+        event.response.emailMessage = `Hey!<p><a href="https://lambda.place4pals.com/public/confirm?username=${event.userName}&code=${event.request.codeParameter}&email=${event.request.email}">Click this link to complete your registration.</a><p>Thanks,<br>place4pals<div style="display:none"><a>${event.request.codeParameter}</a><a>${event.request.codeParameter}</a></div>`;
+
+        return context.done(null, event);
+    }
+    else if (event.triggerSource === 'CustomMessage_ForgotPassword') {
+        event.response.emailSubject = `Reset your password, ${event.request.userAttributes['preferred_username']}!`;
+        event.response.emailMessage = `Hey!<p><a href="https://app.place4pals.com/reset?username=${event.userName}&code=${event.request.codeParameter}&email=${event.request.email}">Click this link to reset your password.</a><p>Thanks,<br>place4pals<div style="display:none"><a>${event.request.codeParameter}</a><a>${event.request.codeParameter}</a></div>`;
+
+        return context.done(null, event);
+    }
+
+
+    if (event.path.startsWith('/public')) {
+        if (event.path.endsWith('/reset')) {
+            const cisp = new aws.CognitoIdentityServiceProvider();
+            let response = await cisp.confirmForgotPassword({ ClientId: process.env.clientId, ConfirmationCode: event.queryStringParameters.code, Password: event.queryStringParameters.password, Username: event.queryStringParameters.username }).promise();
+            console.log(response);
+            return { statusCode: 302, body: null, headers: { 'Access-Control-Allow-Origin': '*', 'Location': 'https://app.place4pals.com' } };
+        }
+        else if (event.path.endsWith('/confirm')) {
+            const cisp = new aws.CognitoIdentityServiceProvider();
+            let response = await cisp.confirmSignUp({ ClientId: process.env.clientId, ConfirmationCode: event.queryStringParameters.code, Username: event.queryStringParameters.username }).promise();
+            console.log(response);
+            return { statusCode: 302, body: null, headers: { 'Access-Control-Allow-Origin': '*', 'Location': `https://app.place4pals.com/login?email=${event.queryStringParameters.email}` } };
+        }
+        else if (event.path.endsWith('/test')) {
+            return {
+                statusCode: 200,
+                body: JSON.stringify('hey there, pal'),
+                headers: { 'Access-Control-Allow-Origin': '*' }
+            };
+        }
+    }
+    else if (event.path.startsWith('/auth')) {
+
     }
 };
