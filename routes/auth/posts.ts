052d96e3b3b92bb07e3bdaa9677b1b00fb41feda
Chris Aitken <chris@heythisischris.com>
2023-04-19 11:39:29 -0400

    Allowing users to upload media via base64 encoded strings

--- routes/auth/posts.ts
+++ routes/auth/posts.ts
@@ -1,5 +1,7 @@
 import { query } from "../../utils/query";
 import { generateId, getId, idToDate, idToOrder } from "../../utils/ksuid";
+import { S3 } from "@aws-sdk/client-s3";
+const s3 = new S3();
 
 export const posts = async ({ event }) => {
   const userId = event?.requestContext?.authorizer?.claims?.profile ?? process.env.mainDynamoDbUserId;
@@ -9,7 +11,7 @@ export const posts = async ({ event }) => {
     // Grab all posts in the default pool
     const posts = await query(
       event.queryStringParameters?.id
-        ? `SELECT * FROM "place4pals" WHERE "parent_id"='user' AND "id"='post#${event.queryStringParameters.id}'`
+        ? `SELECT * FROM "place4pals" WHERE "parent_id"='post' AND "id"='post#${event.queryStringParameters.id}'`
         : `SELECT * FROM "place4pals"."pool_id" WHERE "pool_id"='pool#0' AND begins_with("id",'post#') ORDER BY "id" DESC`,
       100
     );
@@ -29,10 +31,10 @@ export const posts = async ({ event }) => {
     // Perform joins for all usernames
     const userDictionary = Object.fromEntries(
       (
-        await query(`SELECT "id", "name" FROM "place4pals" WHERE "parent_id"='user' AND "id" IN (
+        await query(`SELECT "id", "name", "media" FROM "place4pals" WHERE "parent_id"='user' AND "id" IN (
             ${[...new Set(users)].map((user_id) => `'${user_id}'`).join(",")}
             )`)
-      ).map(({ id, name }) => [id, name])
+      ).map(({ id, name, media }) => [id, { name, media }])
     );
 
     const response = posts.map((post) => ({
@@ -42,9 +44,11 @@ export const posts = async ({ event }) => {
       content: post.content,
       user: {
         id: getId(post.user_id),
-        name: userDictionary[post.user_id],
+        name: userDictionary[post.user_id].name,
+        media: userDictionary[post.user_id].media,
       },
       typing: Array.from(post.typing ?? []),
+      media: post.media,
       comments: post.comments
         .filter(({ parent_comment_id }) => !parent_comment_id)
         .map((comment) => ({
@@ -53,7 +57,8 @@ export const posts = async ({ event }) => {
           content: comment.content,
           user: {
             id: getId(comment.user_id),
-            name: userDictionary[comment.user_id],
+            name: userDictionary[comment.user_id].name,
+            media: userDictionary[comment.user_id].media,
           },
           comments: post.comments
             .filter(({ parent_comment_id }) => comment.id === parent_comment_id)
@@ -63,7 +68,8 @@ export const posts = async ({ event }) => {
               content: comment.content,
               user: {
                 id: getId(comment.user_id),
-                name: userDictionary[comment.user_id],
+                name: userDictionary[comment.user_id].name,
+                media: userDictionary[comment.user_id].media,
               },
               comments: post.comments
                 .filter(
@@ -75,7 +81,8 @@ export const posts = async ({ event }) => {
                   content: comment.content,
                   user: {
                     id: getId(comment.user_id),
-                    name: userDictionary[comment.user_id],
+                    name: userDictionary[comment.user_id].name,
+                    media: userDictionary[comment.user_id].media,
                   },
                 })),
             })),
@@ -89,14 +96,27 @@ export const posts = async ({ event }) => {
     };
   }
   else if (event.httpMethod === 'POST') {
+    let mediaId = null;
+    if (event.body.mediaBase64) {
+      mediaId = generateId();
+      await s3.putObject({
+        Bucket: `p4p-prod-files`,
+        Key: `public/${mediaId}`,
+        ContentEncoding: 'base64',
+        ContentType: event.body.mediaContentType,
+        Body: Buffer.from(event.body.mediaBase64.replace(/^data:.+;base64,/, ""), 'base64'),
+      });
+    }
+
     await query(`INSERT INTO "place4pals" VALUE {
         'parent_id':'post', 
         'id':'post#${generateId()}', 
         'user_id':'user#${userId}',
         'pool_id':'pool#0',
         'name':?,
-        'content':?
-    }`, null, [{ S: event.body.title }, { S: event.body.content }]);
+        'content':?,
+        'media':?
+    }`, null, [{ S: event.body.title }, { S: event.body.content }, { S: mediaId }]);
 
     return {
       statusCode: 200,
--- routes/auth/users.ts
+++ routes/auth/users.ts
@@ -1,6 +1,10 @@
 import { query } from "../../utils/query";
+import { generateId } from "../../utils/ksuid";
+import { S3 } from "@aws-sdk/client-s3";
+const s3 = new S3();
 
 export const users = async ({ event }) => {
+  const userId = event?.requestContext?.authorizer?.claims?.profile ?? process.env.mainDynamoDbUserId;
   if (event.httpMethod === "GET") {
     const response = await query(
       event.queryStringParameters?.id
@@ -11,6 +15,26 @@ export const users = async ({ event }) => {
     return {
       statusCode: 200,
       body: JSON.stringify(response),
+      headers: { "Access-Control-Allow-Origin": "*" },
+    };
+  }
+  else if (event.httpMethod === 'PUT') {
+    let mediaId = null;
+    if (event.body.mediaBase64) {
+      mediaId = generateId();
+      await s3.putObject({
+        Bucket: `p4p-prod-files`,
+        Key: `public/${mediaId}`,
+        ContentEncoding: 'base64',
+        ContentType: event.body.mediaContentType,
+        Body: Buffer.from(event.body.mediaBase64.replace(/^data:.+;base64,/, ""), 'base64'),
+      });
+    }
+    await query(`UPDATE "place4pals" SET "media"='${mediaId}' WHERE "parent_id"='user' AND "id"='user#${userId}'`);
+    
+    return {
+      statusCode: 200,
+      body: true,
       headers: { "Access-Control-Allow-Origin": "*" },
     };
   }
