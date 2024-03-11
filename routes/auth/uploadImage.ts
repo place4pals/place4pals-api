49f47ddeef94e6e10200dfc52a1e965ed4b23f13
Christopher Aitken <chris@heythisischris.com>
2024-03-10 23:37:30 -0500

    Updated username attribute, changed S3 bucket for storing uploaded media

--- routes/auth/uploadImage.ts
+++ routes/auth/uploadImage.ts
@@ -1,4 +1,4 @@
-import { event, getFileExtension } from "#src/utils";
+import { event, getFileExtension, isStaging } from "#src/utils";
 import { randomUUID } from 'crypto'
 import { S3 } from "@aws-sdk/client-s3";
 const s3 = new S3();
@@ -7,7 +7,7 @@ export const uploadImage = async () => {
     const ContentType = event.body.base64.split(';')[0].split('data:')[1];
     const key = `${randomUUID().replaceAll('-', '')}.${getFileExtension(ContentType)}`;
     await s3.putObject({
-        Bucket: 'p4p-prod-files',
+        Bucket: `place4pals-files${isStaging ? '-staging' : ''}`,
         Key: `${key}`,
         ContentEncoding: 'base64',
         ContentType,
--- routes/cognito/postConfirmationConfirmSignUp.ts
+++ routes/cognito/postConfirmationConfirmSignUp.ts
@@ -3,14 +3,14 @@ import { client, event } from "#src/utils";
 const cognito = new CognitoIdentityProvider();
 
 export const postConfirmationConfirmSignUp = async () => {
-    const { sub, email, ['custom:username']: username } = event.request.userAttributes;
+    const { sub, email, nickname } = event.request.userAttributes;
     await cognito.adminUpdateUserAttributes({
-        UserAttributes: [{ Name: 'preferred_username', Value: username }],
+        UserAttributes: [{ Name: 'preferred_username', Value: nickname }],
         UserPoolId: process.env.USER_POOL_ID,
         Username: event.userName
     });
     await client.connect();
-    await client.query(`INSERT INTO "users" ("id","email","name") VALUES ($1, $2, $3)`, [sub, email, username]);
+    await client.query(`INSERT INTO "users" ("id","email","username") VALUES ($1, $2, $3)`, [sub, email, nickname]);
     await client.clean();
     return event;
 }
